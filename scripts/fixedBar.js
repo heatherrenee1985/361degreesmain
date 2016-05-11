7056939a8a0b56fe4e6986608e282ebf33621c80
wodlander <wodlander@gmail.com>
2016-05-11 16:34:17 +0100

    change logo on sticky bar

--- scripts/fixedBar.js
+++ scripts/fixedBar.js
@@ -10,8 +10,10 @@
      Y.on('scroll', function() {
         if (anchorY < window.scrollY) {
             Y.one('#header').addClass('sticky');
+            Y.one('#logoImage img').src = '/assets/logo_green.png';
         } else {
             Y.one('#header').removeClass('sticky');
+            Y.one('#logoImage img').src = '/assets/logo_white.png';
         }
      });
  
