76e34c93d3a681557c5680f4054d79b1e0489f55
roger <wodlander@gmail.com>
2017-05-15 10:17:05 +0100

    added scroll to top anchor

--- scripts/fixedBar.js
+++ scripts/fixedBar.js
@@ -2,30 +2,67 @@
      // Get this value outside of the scroll listener.
      // That way the browser doesn't have to recalculate a million times.
      var anchorY = Y.one('#sticky-anchor').getY();
-     
+
      if (anchorY < window.scrollY) {
             Y.one('#header').addClass('sticky');
      }
- 
+
      Y.on('scroll', function() {
         if (anchorY < window.scrollY) {
             Y.one('#header').addClass('sticky');
             Y.one('#logoImage img').setAttribute('src', '/assets/logo_green.png');
         } else {
             Y.one('#header').removeClass('sticky');
-            var videoItem = Y.one('.collection-type-video.view-item'); 
+            var videoItem = Y.one('.collection-type-video.view-item');
 			if(!videoItem) {
 				Y.one('#logoImage img').setAttribute('src', '/assets/logo_white.png');
 			}
-            
+
         }
      });
- 
+
  }
+
+function showScrollTop() {
+    var body = Y.one(Y.UA.gecko || Y.UA.ie || !!navigator.userAgent.match(/Trident.*rv.11\./) ? 'html' : 'body');
+    Y.on('scroll', function() {
+        if (window.scrollY > 900) {
+           Y.one('#scrollToTop').addClass('show');
+
+        } else {
+            Y.one('#scrollToTop').removeClass('show');
+        }
+    });
+}
+
+function scrollTop() {
+    var scrollTop = Y.one('#scrollToTop');
+    scrollTop.on('click', function() {
+        var a = new Y.Anim({
+            node: Y.one(Y.UA.gecko || Y.UA.ie || !!navigator.userAgent.match(/Trident.*rv.11\./) ? 'html' : 'body'),
+            to: {
+                scrollTop : 0
+            },
+            duration: 2.0,
+            easing: 'easeOut'
+        });
+
+        a.run();
+
+        a.on('end', function () {
+            a.destroy();
+        });
+
+    });
+}
+
+
  // Call the function on domready and window resize, just in case the Y position changes.
  Y.use('node', function() {
      Y.on('domready', function() {
          stickyHeader();
+         showScrollTop();
+         scrollTop();
          Y.on('windowresize', function() {
              stickyHeader();
          });
